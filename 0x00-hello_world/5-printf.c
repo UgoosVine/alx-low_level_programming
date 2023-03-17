66ca75a29a12ea11133b9efb0409aae07841bd38
UgoosVine <divinepraiz@gmail.com>
2023-03-17 09:41:39 -0700

    4-puts.c and 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints "with proper grammer, but the outcome is
+ * a pice of art," followed by newline
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printsf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
