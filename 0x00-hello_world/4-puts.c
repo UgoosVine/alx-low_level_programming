5de21c788808060ee4b1fd6ba41daf0455c51aa0
UgoosVine <divinepraiz@gmail.com>
2023-03-17 08:42:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - the program runs a new line using puts fucntion
+ "
+ * Return: 0, (Great)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"
+return (0);
+}
