6cb3c3306a0ba7cb2b2751b5c488ebe45afb528b
UgoosVine <divinepraiz@gmail.com>
2023-03-18 12:48:33 -0700

    Task 8 solved

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,11 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		putchar(i + '0');
 	}
-	for (j = 'a' <= 'f' ; j++)
+	for (j = 'a' ; j <= 'f' ; j++)
 	{
 		putchar(j);
 	}
