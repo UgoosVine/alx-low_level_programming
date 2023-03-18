1f1068f6d604cc0179b806f80a7a2344cea25224
UgoosVine <divinepraiz@gmail.com>
2023-03-18 09:39:11 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	for (j = 'a' <= 'f' ; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
