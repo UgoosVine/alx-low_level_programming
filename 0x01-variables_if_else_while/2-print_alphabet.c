c1c362c97402a8c2a61b12e3c20fd39416275387
UgoosVine <divinepraiz@gmail.com>
2023-03-18 03:40:34 -0700

    Task two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+	putchar(i);
+	putchar("\n");
+	}
+return (0);
+}
