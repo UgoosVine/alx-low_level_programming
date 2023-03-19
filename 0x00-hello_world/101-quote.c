f22c15909b560c60731a53cc36378e623b2a062b
UgoosVine <divinepraiz@gmail.com>
2023-03-19 07:56:40 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 /**
  * main - "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * follwed by a newline,
- * Return: 1.
+ *
+ * Return: Always 1.
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
