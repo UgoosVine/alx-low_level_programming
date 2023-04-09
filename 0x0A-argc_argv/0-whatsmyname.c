b167d0f75f6a80eefa85e7d82c2b145fa1ed3cc2
UgoosVine <divinepraiz@gmail.com>
2023-04-09 11:32:17 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that prints its name with a newline
+ * @argc: para 1
+ * @argv: para 2
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
