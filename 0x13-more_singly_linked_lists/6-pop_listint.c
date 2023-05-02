3866e0ef4d422db26032033995143155f70d9cd4
UgoosVine <divinepaiz@gmail.com>
2023-05-02 02:29:15 -0700

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes head note of linked list
+ * @head: Pointer to first elment in linked list
+ *
+ * Return: Data that was deleted inside element. or 0
+ * if list empty.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *old;
+	int number;
+
+	if (!head || *head)
+		return(0);
+
+	number = (*head)->n;
+	old = (*head)->next;
+	free(*head);
+	*head = old;
+
+	return (number);
+}
