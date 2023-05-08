241d1014cb6c447a2f682f107cddbc2099347fe8
UgoosVine <divinepaiz@gmail.com>
2023-05-08 13:43:15 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,15 +19,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length =0 ; text_content[length] ;)
+		for (length = 0 ; text_content[length] ;)
 			length++;
 	}
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, length);
 
 	if (op == -1 || wr == -1)
-		return (-1
-				);
+		return (-1);
 	close(op);
 
 	return (1);
